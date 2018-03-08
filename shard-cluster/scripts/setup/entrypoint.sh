e7d7a593be05515c76e0173fd496407f8390366a
Natcha Luangaroonchai <natcha.luangaroonchai@agoda.com>
2018-03-08 13:36:17 +0700

    Update sharding example to MongoDB 3.6 and Docker Compose 3

--- shard-cluster/scripts/setup/entrypoint.sh
+++ shard-cluster/scripts/setup/entrypoint.sh
@@ -1,44 +1,109 @@
 #!/bin/sh
 
-sleep 15
+apt-get update
+apt-get install -y netcat 
 
-mongo --host mongo-rc0svr1 <<EOF
-  rs.initiate({
-    "_id": "rc0",
-    "version": 1,
-    "members": [
-      {
-        "_id": 0,
-        "host": "mongo-rc0svr1",
-        "priority": 1
-      },
-      {
-        "_id": 1,
-        "host": "mongo-rc0svr2",
-        "priority": 0.5,
-        "votes": 1
-      },
-      {
-        "_id": 2,
-        "host": "mongo-rc0svr3",
-        "priority": 0.5,
-        "votes": 1
-      }
+while ! nc -z config0 27019
+do
+  sleep 1
+done
+
+while ! nc -z config1 27019
+do
+  sleep 1
+done
+
+mongo --host config0 --port 27019 <<EOF
+rs.initiate(
+  {
+    _id: "config",
+    configsvr: true,
+    members: [
+      { _id : 0, host : "config0:27019" },
+      { _id : 1, host : "config1:27019" }
     ]
-  });
+  }
+)
 EOF
 
-sleep 15
+while ! nc -z cluster0-shard0-0 27018
+do
+  sleep 1
+done
+
+while ! nc -z cluster0-shard0-1 27018
+do
+  sleep 1
+done
+
+while ! nc -z cluster0-shard0-2 27018
+do
+  sleep 1
+done
+
+mongo --host cluster0-shard0-0 --port 27018 <<EOF
+rs.initiate(
+  {
+    _id : "cluster0-shard0",
+    members: [
+      { _id : 0, host : "cluster0-shard0-0:27018" },
+      { _id : 1, host : "cluster0-shard0-1:27018" },
+      { _id : 2, host : "cluster0-shard0-2:27018" }
+    ]
+  }
+)
+EOF
+
+while ! nc -z cluster1-shard0-0 27018
+do
+  sleep 1
+done
+
+while ! nc -z cluster1-shard0-1 27018
+do
+  sleep 1
+done
+
+while ! nc -z cluster1-shard0-2 27018
+do
+  sleep 1
+done
+
+mongo --host cluster1-shard0-0 --port 27018 <<EOF
+rs.initiate(
+  {
+    _id : "cluster1-shard0",
+    members: [
+      { _id : 0, host : "cluster1-shard0-0:27018" },
+      { _id : 1, host : "cluster1-shard0-1:27018" },
+      { _id : 2, host : "cluster1-shard0-2:27018" }
+    ]
+  }
+)
+EOF
+
+while ! nc -z mongos 27017
+do
+  sleep 1
+done
 
 mongo --host mongos <<EOF
-  sh.addShard("rc0/mongo-rc0svr1");
+sh.addShard("cluster0-shard0/cluster0-shard0-0:27018");
+sh.addShard("cluster1-shard0/cluster1-shard0-0:27018");
 EOF
 
 mongo --host mongos <<EOF
-  use test;
-  db.users.insert({
-    "first_name": "Natcha",
-    "last_name": "Luang - Aroonchai",
-    "email": "natcha.lua@krungthai-axa.co.th",
-  });
+sh.enableSharding("test")
+
+use test;
+db.users.insert({ "hello0": "world!" });
+db.users.insert({ "hello1": "world!" });
+db.users.insert({ "hello2": "world!" });
+db.users.insert({ "hello3": "world!" });
+db.users.insert({ "hello4": "world!" });
+db.users.insert({ "hello5": "world!" });
+db.users.insert({ "hello6": "world!" });
+db.users.insert({ "hello7": "world!" });
+db.users.insert({ "hello8": "world!" });
+db.users.insert({ "hello9": "world!" });
 EOF
